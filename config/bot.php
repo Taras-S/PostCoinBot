5c4834bf009c90833da096ebfe4db8b0b30e6afe
serewann <serewann@gmail.com>
2016-07-30 23:41:54 +0300

    Add new bot settings

--- config/bot.php
+++ config/bot.php
@@ -8,7 +8,30 @@ return [
      * Default amount of sending
      */
     'amount' => 1,
+
+    /**
+     * Max sendings num per day for all teams
+     */
+    'limit' => 1000,
+
+    /**
+     * Max sendings num per day for senders
+     */
+    'senderLimit' => 10,
+
+    /**
+     * Slack related config
+     */
     'slack' => [
+
+        /**
+         * Reaction that allow to send postcoins
+         */
+        'sendingReaction' => 'postcoin',
+
+        /**
+         * Your slash commands token to ensure that requests is valid
+         */
         'slashCommandsToken' => env('SLACK_SLASH_COMMANDS_TOKEN')
     ]
 ];
