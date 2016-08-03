71fee6a852771cdbdcf1623e5989f319d8b20d29
serewann <serewann@gmail.com>
2016-08-03 23:08:29 +0300

    clean up in events to easy implement automatic postoin sending in future

--- app/Events/SendingAdded.php
+++ app/Events/SendingAdded.php
@@ -5,42 +5,21 @@ namespace App\Events;
 use App\Events\Event;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Entities\Sending;
 
-class ReactionAdded extends Event
+class SendingAdded extends Event
 {
     use SerializesModels;
 
-    /**
-     * Who added a reaction?
-     *
-     * @var
-     */
-    public $from_slack_id;
-
-    /**
-     * Who recieved a reaction?
-     *
-     * @var
-     */
-    public $to_slack_id;
-
-    /**
-     * For what type of item reaction was added?
-     *
-     * @var
-     */
-    public $where;
-
+    public $sending;
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct($data)
+    public function __construct(Sending $sending)
     {
-        $this->from_slack_id = $data['user'];
-        $this->to_slack_id = $data['item_user'];
-        $this->where = $data['item']['type'];
+        $this->sending= $sending;
     }
 
     /**
--- app/Listeners/SendingAddedListener.php
+++ app/Listeners/SendingAddedListener.php
@@ -2,15 +2,15 @@
 
 namespace App\Listeners;
 
-use App\Events\ReactionAdded;
-use App\Member;
+use App\Events\SendingAdded;
+use App\Entities\Member;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use App\Sending;
+use App\Entities\Sending;
 use Illuminate\Support\Facades\Response;
 use Slack\Message\Message;
 
-class ReactionAddedListener
+class SendingAddedListener
 {
     /**
      * Create the event listener.
@@ -25,36 +25,11 @@ class ReactionAddedListener
     /**
      * Handle the event.
      *
-     * @param  ReactionAdded  $event
+     * @param SendingAdded $sending
      * @return void
      */
-    public function handle(ReactionAdded $reaction, SlackChat $chat, SlackAPI $slack)
+    public function handle(SendingAdded $sending)
     {
-        if ($reaction->from_slack_id == $reaction->to_slack_id) return;
-
-        $sending = new Sending([
-                'amount' => config('bot.amount'),
-                'from_slack_id' => $reaction->from_slack_id,
-                'to_slack_id' => $reaction->to_slack_id,
-                'where' => $reaction->where,
-                'type' => 'reaction',
-                'done' => false
-            ]
-        );
-        $sending->save();
-
-        $this->notifyIfWalletIsNotSet($sending->to_slack_id, $slack, $chat);
-    }
-
-
-    protected function notifyIfWalletIsNotSet($slack_id, $slack, $chat)
-    {
-        if (Member::where('slack_id', $slack_id)->first()->wallet == NULL) {
-            $message = $chat->client->getMessageBuilder()->setText(
-                Response::view('bot.response.chat.walletNotSet')
-            )->setChannel($slack->getDMByUserId($slack_id));
-
-            $chat->client->sendMessage($message);
-        }
+        // TODO: actually send postcoin when sended added
     }
 }
