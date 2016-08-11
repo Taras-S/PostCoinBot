34706b17e293818636734b198652adb37ea1d220
serewann <serewann@gmail.com>
2016-08-11 23:00:41 +0300

    Move reusable parts to bot helper

--- app/Services/Bot/Helper.php
+++ app/Services/Bot/Helper.php
@@ -2,7 +2,54 @@
 
 namespace App\Services\Bot;
 
+/**
+ * Bot helper functions
+ *
+ * @package App\Services\Bot
+ */
 class Helper
 {
-    // TODO: make helper functions for bot
+    /**
+     * Where bot response views is stored
+     *
+     * @var string
+     */
+    protected $basePath = 'bot.responses';
+
+    /**
+     * Returns chat reponse
+     *
+     * @param $template
+     * @param $payload
+     * @return string
+     */
+    public function chatResponse($template, $payload = [])
+    {
+        return $this->getResponse('chat', $template, $payload);
+    }
+
+    /**
+     * Returns command response
+     *
+     * @param $template
+     * @param $payload
+     * @return string
+     */
+    public function commandResponse($template, $payload = [])
+    {
+        return $this->getResponse('commands', $template, $payload);
+    }
+
+    /**
+     * Returns response
+     *
+     * @param $path
+     * @param $template
+     * @param $payload
+     * @return string
+     */
+    protected function getResponse($path, $template, $payload)
+    {
+        return (string) view($this->basePath .'.' . $path . '.' . $template, $payload);
+    }
 }
