3a470085f9685aa394fd23ac003d6a5dd5a4946a
serewann <serewann@gmail.com>
2016-08-21 18:06:43 +0300

    add slack ssl verification mode

--- app/Http/routesApi.php
+++ app/Http/routesApi.php
@@ -14,10 +14,18 @@ Route::group(['namespace' => 'Bot'], function() {
     /**
      * Slack slash commands
      */
-    Route::post('/api/slack/command/call', 'Commands\SlackCommandController@call');
+    Route::post('/api/slack/command/call', 'Commands\SlackCommandController@call');\
 
     /**
      * Slack Events
      */
     Route::post('/api/slack/event/fire', 'SlackSendingReactionEventListener@fire')->middleware('sendingReaction');
+
+    /**
+     * Slack SSL verification
+     */
+    Route::get('/api/slack/{type}/{action}', function ($type, $action)
+    {
+        return Response('OK', 200);
+    });
 });
